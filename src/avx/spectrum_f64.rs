6d7e7d76c43695a568c3f6bec9918146b951fe25
Radzivon Bartoshyk <radzivon.bartoshyk@proton.me>
2025-12-17 21:29:59 +0000

    Initial implementation

--- src/avx/spectrum_f64.rs
+++ src/avx/spectrum_f64.rs
@@ -26,7 +26,6 @@
  * // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-use crate::avx::AvxSpectrumF32;
 use crate::spetrum_arith::SpectrumArithmetic;
 use num_complex::Complex;
 use std::arch::x86_64::*;
